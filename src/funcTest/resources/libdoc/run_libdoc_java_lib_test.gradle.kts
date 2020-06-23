a901f0b4c5d68fcece78aac4716678bbfe71e5ec
Mathias Süß <270275+mathze@users.noreply.github.com>
2020-06-23 17:34:04 +0200

    #6 added missing test files for libdoc

---
+++ src/funcTest/resources/libdoc/run_libdoc_java_lib_test.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+  id("de.qualersoft.robotframework")
+  `java`
+}
+
+repositories {
+  mavenLocal()
+  mavenCentral()
+}
+
+tasks.register<de.qualersoft.robotframework.gradleplugin.tasks.LibdocTask>("libdocRun") {
+  libdoc {
+    outputFile = file("libdoc.html")
+    libraryOrResourceFile = "ALib"
+    additionalPythonPaths = files(project.tasks.withType<Jar>().first().outputs.files, project.configurations.getByName("runtimeClasspath").files)
+  }
+}
+
+dependencies {
+  implementation(group = "org.robotframework", name = "javalib-core", version = "2.0.3")
+}
